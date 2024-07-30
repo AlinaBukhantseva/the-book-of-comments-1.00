8c43b70536531c826bf07709df0cd82a86ee24fc
Alina Bukhantseva <alinabukhantseva@Alinas-iMac.local>
2024-07-29 23:29:31 -0400

    comment edited

--- models/Comment.js
+++ models/Comment.js
@@ -1,30 +1,25 @@
-const mongoose = require('mongoose')
-const {model, Schema} = require('mongoose')
+const { model, Schema} = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Schema.Types.String.set('trim', true);
 
-const commentSchema = new Schema({
-   _id:  mongoose.Schema.Types.ObjectId,
-   user: Schema.Types.ObjectId,
+const commentSchema =
 
-    createdAt: {
-        type: String,
-        trim: true,
-    },
-    rating: {
-        type: Number,
-        trim: true,
-    },
-    title: {
-        type: String,
-        trim: true,
-    },
-    description: {
-        type: String,
-        trim: true,
-    }
+    new Schema({
+        _id: mongoose.Schema.Types.ObjectId,
 
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
 
+        createdAt: String,
 
-})
+        rating: Number,
+
+        title: String,
+
+        description: String,
+
+    });
 
 module.exports = model('Comment', commentSchema)
